ceba3e28acf89a3d48d2a60d492caf541739783f
Diogo Ribeiro de Oliveira <diogo.ribo@gmail.com>
2020-07-31 15:20:13 -0300

    Criando classe pessoa com validacoes

---
+++ src/main/java/dev/diogoro/lyncastest/domain/Pessoa.java
@@ -0,0 +1,50 @@
+package dev.diogoro.lyncastest.domain;
+
+import java.sql.Timestamp;
+import java.sql.Date;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Pessoa {
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	@Type(type="org.hibernate.type.UUIDCharType")
+	@Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+	private UUID id;
+	
+	private String nome;
+	private Character sexo;
+	private String email;
+	private Date dataNascimento;
+	private String naturalidade;
+	@CreationTimestamp
+	@Column(updatable = false)
+	private Timestamp dataCriacao;
+	@UpdateTimestamp
+	private Timestamp dataUltimaAtualizacao;
+	@Column(unique = true)
+	private String cpf;
+
+}
