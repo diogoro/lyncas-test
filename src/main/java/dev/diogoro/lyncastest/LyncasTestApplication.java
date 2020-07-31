7a6f62af81d1aabf0862db62a20774518326d349
Diogo Ribeiro de Oliveira <diogo.ribo@gmail.com>
2020-07-31 14:47:55 -0300

    Criacao do projeto

---
+++ src/main/java/dev/diogoro/lyncastest/LyncasTestApplication.java
@@ -0,0 +1,13 @@
+package dev.diogoro.lyncastest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyncasTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyncasTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/diogoro/lyncastest/LyncasTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.diogoro.lyncastest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyncasTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
