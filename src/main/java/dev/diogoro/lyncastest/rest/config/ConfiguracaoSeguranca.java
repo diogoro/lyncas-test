905ea3c180dba88f5f1541719bec6f24c41e91e8
Diogo Ribeiro de Oliveira <diogo.ribo@gmail.com>
2020-08-01 01:25:50 -0300

    Adicionando autenticacao basic ao endpoint pessoa

---
+++ src/main/java/dev/diogoro/lyncastest/rest/config/ConfiguracaoSeguranca.java
@@ -0,0 +1,26 @@
+package dev.diogoro.lyncastest.rest.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Order(1)
+public class ConfiguracaoSeguranca extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/source").permitAll()
+		.anyRequest().authenticated().and().httpBasic();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}password").roles("USER");
+	}
+}
