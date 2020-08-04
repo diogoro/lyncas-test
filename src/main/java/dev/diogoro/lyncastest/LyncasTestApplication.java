4800c0d6149c6c84c76901b7879bfd480b6cbe31
Diogo Ribeiro de Oliveira <diogo.ribo@gmail.com>
2020-08-03 22:09:35 -0300

    Configurando o timezone da aplicacao

--- src/main/java/dev/diogoro/lyncastest/LyncasTestApplication.java
+++ src/main/java/dev/diogoro/lyncastest/LyncasTestApplication.java
@@ -1,11 +1,20 @@
 package dev.diogoro.lyncastest;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LyncasTestApplication {
 
+	@PostConstruct
+    void started() {
+      TimeZone.setDefault(TimeZone.getTimeZone("GMT-3"));
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LyncasTestApplication.class, args);
 	}
