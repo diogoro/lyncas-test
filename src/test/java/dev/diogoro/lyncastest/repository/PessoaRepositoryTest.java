721fb8c68efbe1a5e32459ad50bcbed4cf9f6b2d
Diogo Ribeiro de Oliveira <diogo.ribo@gmail.com>
2020-08-03 23:05:28 -0300

    Alteracao para usar mysql

--- src/test/java/dev/diogoro/lyncastest/repository/PessoaRepositoryTest.java
+++ src/test/java/dev/diogoro/lyncastest/repository/PessoaRepositoryTest.java
@@ -11,11 +11,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 import dev.diogoro.lyncastest.BaseTest;
 import dev.diogoro.lyncastest.domain.Pessoa;
 
 @DataJpaTest
+@ActiveProfiles("testH2")
 class PessoaRepositoryTest extends BaseTest{
 
 	@Autowired
